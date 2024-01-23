0d3b2ddf867d10843df61c381f5fb59fb2ca1c31
matthewdillender <matthewdillender@gmail.com>
2024-01-23 01:03:04 -0800

    Actualize Career Inventory

---
+++ 204.rb
@@ -0,0 +1,12 @@
+# QUESTION 1
+# Paste this link in your browser to make a copy of the Career Inventory Google document: 
+#     https://docs.google.com/document/d/1XaPaG6QXSQEajkHE1noa5pMfa27qmegSOY_FY6zBoQA/copy  
+# Fill in the answers for each question in the Career Inventory.
+# Change the sharing settings of the document to "Anyone with the link can view" 
+# Copy the link to your Career Inventory Google document and paste it below.
+
+https://docs.google.com/document/d/1cdy7bIjG9Sw_WrzRssiz7zjc_37VYIsd01xyIIcioAk/edit?pli=1#heading=h.gyzkhjev28mz
+
+git add --all
+git commit -m "Add practice problem"
+git push origin main
