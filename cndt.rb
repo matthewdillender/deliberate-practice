d74420158fb872e270be1f5280b2600ae6fa8c63
matthewdillender <matthewdillender@gmail.com>
2024-01-27 10:08:00 -0800

    add practice

--- cndt.rb
+++ cndt.rb
@@ -21,3 +21,12 @@
 # If the total order value is between $50 and $100, the discount is 5% for regular customers and 10% for premium customers.
 # If the total order value is greater than $100, the discount is 10% for regular customers and 15% for premium customers.
 
+
+# Write a Ruby program that stores the weight of a package and the destination (domestic or international). Then calculate the shipping fee based on the following conditions:
+
+# If the destination is domestic:
+# If the weight is less than or equal to 1 kg, the shipping fee is $5.
+# If the weight is greater than 1 kg, the shipping fee is $10.
+# If the destination is an international shipment:
+# If the weight is less than or equal to 1 kg, the shipping fee is $15.
+# If the weight is greater than 1 kg, the shipping fee is $25.
