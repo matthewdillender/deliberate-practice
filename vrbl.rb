13f066f9b654ad31765e4df6ec75d230ba690118
matthewdillender <matthewdillender@gmail.com>
2024-01-27 09:45:08 -0800

    Add practice

---
+++ vrbl.rb
@@ -0,0 +1,5 @@
+# Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+
+x = "Billy"
+y = " Bumpis"
+puts x + y
