edabccf3f1737c202040b5fdaebc6ab9b9193837
matthewdillender <matthewdillender@gmail.com>
2024-01-27 10:01:43 -0800

    Add practice

---
+++ arry.rb
@@ -0,0 +1,31 @@
+# 04-arrays-hashes1.md
+
+# Create an array to store 3 words. Then add two more words to the array and print the array on one line.
+
+
+# Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
+
+
+# Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
+
+
+# Create an array to store 1 number. Then add three more numbers to the array and print the array on one line.
+
+
+# Create an array to store 3 strings with lower case letters. Then change the third string to have all capital letters and print the array on one line.
+
+
+# Create an array to store 3 names. Then print out each name on separate lines with a while loop.
+
+
+# Create an array to store 2 strings. Then add one string to the array and print the array on one line.
+
+
+# Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
+
+
+# Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
+
+
+# Create an array to store names of 3 different countries. Then add one more country and print the array one line.
+
