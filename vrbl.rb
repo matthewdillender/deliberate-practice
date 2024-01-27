92255ae22d419e35440332288e6176acb7ec12b9
matthewdillender <matthewdillender@gmail.com>
2024-01-27 09:53:24 -0800

    Add practice

--- vrbl.rb
+++ vrbl.rb
@@ -3,3 +3,20 @@
 x = "Billy"
 y = " Bumpis"
 puts x + y
+
+# Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+
+# Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+
+# Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
+
+# Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
+
+# Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
+
+# Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
+
+# Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
+
+# Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
+
