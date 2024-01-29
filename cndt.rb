f147cd1c1ab9536f20a647d5ae2f4945fa62a2c0
matthewdillender <matthewdillender@gmail.com>
2024-01-28 21:00:40 -0800

    Add practice

--- cndt.rb
+++ cndt.rb
@@ -2,6 +2,25 @@
 
 # 1: Write a program that stores a customer's age and a movie's time in two separate variables. Then calculate the price of a movie ticket based on the following conditions:
 
+a = age
+t = times
+
+age = 50
+time = 12
+if age <= 12
+  ticket_price = 5
+elsif age >= 13 && age <= 59
+  if time < 18
+    ticket_price = 7
+  else
+    ticket_price = 10
+  end
+elsif age >= 60
+  ticket_price = 7
+end
+
+puts "Ticket price: $#{ticket_price}"
+
 # If the age is 12 years old or younger, the ticket price is $5.
 # If the age is between 13 and 59 years old and the movie is before 6 PM, the ticket price is $7. After 6 PM, the ticket price is $10.
 # If the customer is 60 years old or older, the ticket price is $7.
