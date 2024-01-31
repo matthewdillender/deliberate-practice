55bc7009f6bcba6c38207317bc6a9f6491788c45
matthewdillender <matthewdillender@gmail.com>
2024-01-31 06:33:30 -0800

    Add practice

---
+++ 312.rb
@@ -0,0 +1,21 @@
+# QUESTION 1
+
+# Use a loop to print out only the numbers greater than 10.
+
+index = 0
+numbers = [4, 53, 22, 3, 9, 7, 12]
+
+while index <= numbers.length
+  number = numbers[index]
+  index = index + 1
+  if number > 10
+  puts number
+  end
+end
+
+pp numbers
+
+
+
+
+
