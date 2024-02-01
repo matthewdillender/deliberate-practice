e404b7e66877fb4021f33b55c4be20882ab3ff4d
matthewdillender <156735514+matthewdillender@users.noreply.github.com>
2024-02-01 11:31:48 -0800

    Add practice

--- arry.rb
+++ arry.rb
@@ -2,51 +2,53 @@
 
 # Create an array to store 3 words. Then add two more words to the array and print the array on one line.
 
-array = ["Wants", "Needs", "Desires"]
-array.push("Greed")
-array.push("Peace")
-pp array
+# array = ["Wants", "Needs", "Desires"]
+# array.push("Greed")
+# array.push("Peace")
+# pp array
 
 # Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
 
-letters = ["a", "b", "c", "d"]
-letters[1] = 2
-pp letters
+# letters = ["a", "b", "c", "d"]
+# letters[1] = 2
+# pp letters
 
 # Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
 
-numbers = [1, 2, 3, 4, 5]
-index = 0
-while index < numbers.length
-  puts numbers[index]
-  index = index + 1
-end
+# numbers = [1, 2, 3, 4, 5]
+# index = 0
+# while index < numbers.length
+#   puts numbers[index]
+#   index = index + 1
+# end
 
 # Create an array to store 1 number. Then add three more numbers to the array and print the array on one line.
 
-numbers = [1]
-numbers.push(5)
-numbers.push(6)
-numbers.push(7)
-pp numbers
+# numbers = [1]
+# numbers.push(5)
+# numbers.push(6)
+# numbers.push(7)
+# pp numbers
 
 # Create an array to store 3 strings with lower case letters. Then change the third string to have all capital letters and print the array on one line.
 
-letters = ["chill", "relax", "mellow"]
-letters[2] = "mellow".upcase
-pp letters
+# letters = ["chill", "relax", "mellow"]
+# letters[2] = "mellow".upcase
+# pp letters
 
 # Create an array to store 3 names. Then print out each name on separate lines with a while loop.
 
-names = ["Jon", "Bill", "Ted"]
-index = 0
-while index < names.length
-  puts names[index]
-  index = index + 1
-end
+# names = ["Jon", "Bill", "Ted"]
+# index = 0
+# while index < names.length
+#   puts names[index]
+#   index = index + 1
+# end
 
 # Create an array to store 2 strings. Then add one string to the array and print the array on one line.
 
+monkey = ["mon", "key"]
+pp monkey.push("banana")
 
 # Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
 
